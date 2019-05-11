50c65d2819bb7eae976d2c6a83707f8dfb681c3c
Jason  Montes <jasone.montes@gmail.com>
2019-05-11 06:40:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+end
+
+class Dog
+def name=(dog_name)
+  @name= dog_name
+end
+end
+
+class Dog
+def name
+  @name
+end
+end
+
+class Dog
+  def breed=(dog_breed)
+    @breed= dog_breed
+   end
+  end
+  
+  class Dog
+    def breed
+      @breed
+    end
+  end 
---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+end
+
+class Person
+  def name=(name_of_person)
+    @name= name_of_person
+  end
+end
+
+class Person
+  def name
+    @name
+  end
+end
+
+class Person
+  def job=(job_of_person)
+    @job= job_of_person
+  end
+end
+
+class Person
+  def job
+    @job
+  end
+end
